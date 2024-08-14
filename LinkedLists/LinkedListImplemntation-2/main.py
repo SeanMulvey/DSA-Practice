fa3f665cd67089f7e0dbbd097857f24ea5632ce1
SeanMulvey <143544386+SeanMulvey@users.noreply.github.com>
2024-08-13 23:15:02 -0700

    Started second implementation of linked lists

---
+++ LinkedLists/LinkedListImplemntation-2/main.py
@@ -0,0 +1,4 @@
+# Building on to the what we learned with LinkedListImplementation-1
+# We will do a more complex practical example where we set up functions for a linked list, rather than hard coding one
+######################################################################################################################
+
