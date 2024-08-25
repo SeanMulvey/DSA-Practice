35d979d5994de39aea449d8827b673df10d687df
mulveys7619 <47682134+mulveys7619@users.noreply.github.com>
2024-08-25 19:10:11 -0400

    Binary Calculator practice in Python(Still working on base10 to Binary Converter)

---
+++ Base10ToBinaryCalculator/main.py
@@ -0,0 +1,19 @@
+def main():
+    val = input("Enter an integer between 0 and 255 to convert to binary: ")
+
+    if not val.isnumeric():
+        print("Enter a valid integer between 0 and 255!")
+        main()
+    val = int(val)
+    if val > 255 or val < 0:
+        print("Enter a valid integer between 0 and 255!")
+        main()
+    print(performCalculation(val))
+    main()
+
+
+
+def performCalculation(val):
+   pass
+
+main()
