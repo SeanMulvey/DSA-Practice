e9fb8dbe8087e614e79b80cbf82af89b5c7c03e2
mulveys7619 <47682134+mulveys7619@users.noreply.github.com>
2024-08-25 22:26:22 -0400

    Tried a different method for conversion. Still need to work on it :/

--- Base10ToBinaryCalculator/main.py
+++ Base10ToBinaryCalculator/main.py
@@ -5,6 +5,7 @@ def main():
         print("Enter a valid integer between 0 and 255!")
         main()
     val = int(val)
+
     if val > 255 or val < 0:
         print("Enter a valid integer between 0 and 255!")
         main()
@@ -14,6 +15,22 @@ def main():
 
 
 def performCalculation(val):
-   pass
+    stack = []
+    res = ""
+    e = 7
+    while e > 0:
+        print(val)
+        if val - 2**e > 0:
+           stack.append("1")
+           val -= 2 ** e
+        else:
+           stack.append("0")
+        e -= 1
+    
+    # while stack:
+    #     res += stack.pop()
+    return int(stack)
+       
+
 
 main()
